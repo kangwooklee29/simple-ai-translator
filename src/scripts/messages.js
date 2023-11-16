e430c0ab69b450521f58bf610085755003b89341
kangwooklee29 <kangwooklee29@gmail.com>
2023-11-16 23:01:52 +0900

    Add logs before send prompts to OpenAI

--- src/scripts/messages.js
+++ src/scripts/messages.js
@@ -5,6 +5,7 @@ export class Messages{
     }
 
     async send_chatgpt(content) {
+        console.log(localStorage.getItem("target_language"), content);
         await chatgpt_api([
             {role:"user", content:""}, 
             {role: "user", content: `Translate this text into ${localStorage.getItem("target_language")}: "${content}"`}]);
